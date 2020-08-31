629ad03b84e12dbf73d4fd6693e49f8379971bb9
Deepshikha <deepshika3010@gmail>
2020-08-31 15:04:52 +0530

    initial commit

---
+++ javscript/basicjs.js
@@ -0,0 +1,274 @@
+/*-------Map-----
+ const arr = [{name:"Deepshikha",age:18}, {name:"Manu",age:20}]
+ const person_names = arr.map(person => person.name)
+ console.log(person_names);
+ */
+
+/*--filter------
+const fruits = [
+  {name:"banana",color:"yellow"},
+  {name:"watermelon",color:"red"},
+  {name:"orange",color:"orange"}
+ ]
+ const filteredFruits = fruits.filter(fruit => fruit.color === "yellow");
+ console.log(filteredFruits)
+*/
+
+/*----- find-----
+
+let x = [
+  {name:"jay",age:1},
+  {name:"hulu",age:30}
+ ]
+ let d= x.find(obj => obj.name === "jay");
+
+console.log(d);
+*/
+//---some---//
+
+
+// let names = ["deep","shikha","pandey"];
+// let f=names.some(name => name.length > 4)
+// console.log(f);
+
+//----Every---//
+// let names = ["deep","shikha","pandey"];
+// let f=names.every(name => name.length > 4)
+//  console.log(f);
+
+//---Reduce---//
+//const numbers = [1,2,3,4,5]
+//let sum = 0
+//for(let i=0;i<numbers.length;i++){
+//   sum += numbers[i]
+//}
+//console.log(sum);
+//let R= numbers.reduce((sum, numbers) => sum + numbers, 0);
+//console.log(R);
+
+
+//---For Each method----//
+
+// const arr = [1,2,3,4]
+// arr.forEach(item => console.log(item))
+
+/* let arr1 = [{name:"deepshikha",age:23}, {name:"shibu",age:23}]
+let temp = []
+for(let i = 0; i < arr1.length; i++){
+    temp.push(arr1[i].name)
+}
+console.log(temp);
+alt+shift+a//shortcut of multiline comment
+ */
+
+
+//---findIndex----//
+/*var ages = [3, 10, 18, 20];
+
+var found=ages.find(Element=>Element<10);
+
+
+console.log(found);
+
+*/
+
+//----includes----//
+/*var str = "Hello world, welcome to the universe.";
+var n = str.includes("world");
+
+console.log(n);*/
+
+//--------key value entity------//
+
+
+/*let users=[
+  {name:"deep",
+  id:123456,
+   salary:1234567,
+   dapatment:"java devloper "
+},
+{name:"shibu",
+  id:123456,
+   salary:09876543,
+   dapatment:"fullstack developer "
+},
+]
+console.log(Object.values(users))
+console.log(Object.keys(users))
+console.log(Object.entries(users))
+*/
+//let entries=Object.entries(...users);
+//console.log(entries);
+
+//let entries=Object.Value(...users);
+//console.log(entries);
+
+
+//-------splice------//
+
+/*---var frits=["banana","kiwi","Apple"]
+
+frits./*-------Map-----
+ const arr = [{name:"Deepshikha",age:18}, {name:"Manu",age:20}]
+ const person_names = arr.map(person => person.name)
+ console.log(person_names);
+ */
+
+/*--filter------
+const fruits = [
+  {name:"banana",color:"yellow"},
+  {name:"watermelon",color:"red"},
+  {name:"orange",color:"orange"}
+ ]
+ const filteredFruits = fruits.filter(fruit => fruit.color === "yellow");
+ console.log(filteredFruits)
+*/
+
+/*----- find-----
+
+let x = [
+  {name:"jay",age:1},
+  {name:"hulu",age:30}
+ ]
+ let d= x.find(obj => obj.name === "jay");
+
+console.log(d);
+*/
+//---some---//
+
+
+// let names = ["deep","shikha","pandey"];
+// let f=names.some(name => name.length > 4)
+// console.log(f);
+
+//----Every---//
+// let names = ["deep","shikha","pandey"];
+// let f=names.every(name => name.length > 4)
+//  console.log(f);
+
+//---Reduce---//
+//const numbers = [1,2,3,4,5]
+//let sum = 0
+//for(let i=0;i<numbers.length;i++){
+//   sum += numbers[i]
+//}
+//console.log(sum);
+//let R= numbers.reduce((sum, numbers) => sum + numbers, 0);
+//console.log(R);
+
+
+//---For Each method----//
+
+// const arr = [1,2,3,4]
+// arr.forEach(item => console.log(item))
+
+/* let arr1 = [{name:"deepshikha",age:23}, {name:"shibu",age:23}]
+let temp = []
+for(let i = 0; i < arr1.length; i++){
+    temp.push(arr1[i].name)
+}
+console.log(temp);
+alt+shift+a//shortcut of multiline comment
+ */
+
+
+//---findIndex----//
+/*var ages = [3, 10, 18, 20];
+
+var found=ages.find(Element=>Element<10);
+
+
+console.log(found);
+
+*/
+
+//----includes----//
+/*var str = "Hello world, welcome to the universe.";
+var n = str.includes("world");
+
+console.log(n);*/
+
+//--------key value entity------//
+
+
+/*let users=[
+  {name:"deep",
+  id:123456,
+   salary:1234567,
+   dapatment:"java devloper "
+},
+{name:"shibu",
+  id:123456,
+   salary:09876543,
+   dapatment:"fullstack developer "
+},
+]
+console.log(Object.values(users))
+console.log(Object.keys(users))
+console.log(Object.entries(users))
+*/
+//let entries=Object.entries(...users);
+//console.log(entries);
+
+//let entries=Object.Value(...users);
+//console.log(entries);
+
+
+//-------splice------//
+
+/*---var frits=["banana","kiwi","Apple"]
+
+frits.splice(2,1,"lemon","Mango")
+console.log(frits);
+
+*/
+/*--------push------
+
+let array=["manu","viju","deepu","shibu"]
+array.push("deep")
+
+console.log(array);
+array.pop("deep")
+console.log(array);
+
+*/
+
+
+/*------shift and unshift-----
+
+let array=["manu","viju","deepu","shibu"]
+
+array.shift();
+array.unshift();
+console.log(array);
+
+
+
+
+splice(2,1,"lemon","Mango")
+console.log(frits);
+*/
+
+/*--------push------
+
+let array=["manu","viju","deepu","shibu"]
+array.push("deep")
+
+console.log(array);
+array.pop("deep")
+console.log(array);
+
+*/
+
+
+/*------shift and unshift-----*/
+
+let array=["manu","viju","deepu","shibu"]
+
+array.shift();
+array.unshift();
+console.log(array);
+
+
+
+
